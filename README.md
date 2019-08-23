a74bfa06a964a646ebf7fa2c4cc4970c076acf1f
Compolomus <compolom@gmail.com>
2019-08-23 15:48:56 +0200

    Init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Compolomus Collection
+
+[![License](https://poser.pugx.org/compolomus/Collection/license)](https://packagist.org/packages/compolomus/Collection)
+
+[![Build Status](https://scrutinizer-ci.com/g/Compolomus/Collection/badges/build.png?b=master)](https://scrutinizer-ci.com/g/Compolomus/Collection/build-status/master)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/Compolomus/Collection/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/Compolomus/Collection/?branch=master)
+[![Code Coverage](https://scrutinizer-ci.com/g/Compolomus/Collection/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/Compolomus/Collection/?branch=master)
+[![Code Climate](https://codeclimate.com/github/Compolomus/Collection/badges/gpa.svg)](https://codeclimate.com/github/Compolomus/Collection)
+[![Downloads](https://poser.pugx.org/compolomus/Collection/downloads)](https://packagist.org/packages/compolomus/Collection)
+
+## Установка:
+
+composer require compolomus/Collection
+
+## Применение:
+
+```php
+
+use Compolomus\Collection\Collection;
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+
+```
---
+++ src/Collection.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+
+namespace Compolomus\Collection;
+
+use InvalidArgumentException;
+
+class Collection
+{
+    public const ASC = 1;
+    public const DESC = 0;
+
+    private $generic;
+
+    private $collection = [];
+
+    public function __construct(string $class)
+    {
+        $this->generic = $class;
+    }
+
+    public function addOne(object $object): self
+    {
+        if (!($object instanceof $this->generic)) {
+            throw new InvalidArgumentException('Object mast be instanceof ' . $this->generic . ' class ' . get_class($object) . ' given');
+        }
+        $this->collection[] = $object;
+
+        return $this;
+    }
+
+    public function addAll(array $objects): self
+    {
+        array_map([$this, 'addOne'], $objects);
+
+        return $this;
+    }
+
+    public function immutable(): self
+    {
+        return clone $this;
+    }
+
+    private function cmp(string $key, int $order): callable
+    {
+        return static function(object $a, object $b) use ($key, $order) {
+            return $order ? $a->$key <=> $b->$key : $b->$key <=> $a->$key;
+        };
+    }
+
+    public function sort(string $key, int $order = Collection::ASC): self
+    {
+        uasort($this->collection, $this->cmp($key, $order));
+
+        return $this;
+    }
+
+    public function limit(int $limit, int $offset = 0): self
+    {
+        $this->collection = array_slice($this->collection, $offset, $limit);
+
+        return $this;
+    }
+
+    public function get(): array
+    {
+        return $this->collection;
+    }
+}
